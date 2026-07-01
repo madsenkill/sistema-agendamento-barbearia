f165272893c638d7572343bf0a422306aa534dcb
Matheus Madsen <matheusmadsen22@gmail.com>
2026-07-01 09:46:39 -0300

    Migrando o sistema para banco de dados SQLite3

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
 from flask import Flask, render_template, request, jsonify
+import sqlite3
 
+# --- Bloco do Banco de Dados entrando antes do app = Flask(--name--) ---
+def iniciar_banco():
+    conexao = sqlite3.connect("barbearia.db")
+    cursor = conexao.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS agendamentos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT ,
+            nome TEXT NOT NULL,
+            servico TEXT NOT NULL,
+            horario TEXT NOT NULL
+        )
+    """)
+    conexao.commit()
+    conexao.close()
+    
+# Executa a criação do banco antes do servidor ligar    
+iniciar_banco()
 
 def pegar_horarios_ocupados():
-    horarios_ocupados = []
-    with open('agendamentos.txt', 'r', encoding='utf-8') as arquivo:
-        for linha in arquivo:
-            if "Horário:" in linha:
-                partes = linha.split("Horário: ")
-                horario_limpo = partes[1].strip()
-                horarios_ocupados.append(horario_limpo)
+    # 1. Ligamos para o banco de dados
+    conexao = sqlite3.connect("barbearia.db")
+    cursor = conexao.cursor()
     
-    return horarios_ocupados  # Esse "return" faz a máquina "cuspir" a lista prontinha para quem chamar ela!
+    # 2. Fazer a query SQL para pegar apenas a coluna "horario" de todos os agendamentos
+    cursor.execute("SELECT horario FROM agendamentos")
+    linhas = cursor.fetchall()  # Puxa todas as linhas encontradas no banco
+    
+    conexao.close()
+    
+    # 3. Limpamos os dados usando o mesmo nome de variável (linha)
+    horarios_ocupados = [linha[0] for linha in linhas]
+    
+    return horarios_ocupados
 app = Flask(__name__)
 
 # Rota 1: Entrega a página visual para o cliente
@@ -28,13 +51,23 @@ def salvar_agendamento():
     dados = request.json
     nome_cliente = dados.get('nome')
     servico_escolhido = dados.get('servico')
-    horario_escolhido = dados.get('horario') # <-- Pegando o horário novo!
+    horario_escolhido = dados.get('horario') 
 
-    # Adicionando o horário na gravação do arquivo de texto
-    with open('agendamentos.txt', 'a', encoding='utf-8') as arquivo:
-        arquivo.write(f"Cliente: {nome_cliente} | Serviço: {servico_escolhido} | Horário: {horario_escolhido}\n")
-
-    print(f"🚀 Agendamento salvo com sucesso: {nome_cliente} às {horario_escolhido}")
+    # Ligando o banco de dados
+    conexao = sqlite3.connect("barbearia.db")
+    cursor = conexao.cursor()
+    
+    # Usando o comando SQL INSERT INTO para injetar os dados nas colunas certas
+    cursor.execute("""
+        INSERT INTO agendamentos (nome, servico, horario)
+        VALUES (?, ?, ?)
+    """, (nome_cliente, servico_escolhido, horario_escolhido))
+    
+    # Salvando as alterções e fechando a conexão com segurança
+    conexao.commit()
+    conexao.close()
+    print(f" Sucesso! {nome_cliente} gravado direto no Banco de Dados!")
+    
     return jsonify({"status": "sucesso", "mensagem": "Agendamento gravado!"})
 
 if __name__ == '__main__':
--- teste_logica.py
+++ teste_logica.py
@@ -1,9 +1,25 @@
-horarios_ocupados = []
+def limpar_preco (preco_texto):
+    return int(preco_texto.replace("R$ ", ""))
 
-with open('agendamentos.txt', 'r', encoding='utf-8') as arquivo:
-    for linha in arquivo:
-        if "Horário:" in linha:
-            partes = linha.split("Horário: ")
-            horario_limpo = partes[1].strip()
-            horarios_ocupados.append(horario_limpo)
-print("Horários que já foram agendados hoje:", horarios_ocupados)
+agendamentos_brutos = [
+    ["mateus", "R$ 25"],
+    ["juan", "R$ 40"],
+    ["john", "R$ 40"]
+]
+
+faturamento_total = 0
+
+for agendamento in agendamentos_brutos:
+    nome_sujo = agendamento[0]
+    preco_sujo = agendamento[1]
+    
+    nome_limpo = nome_sujo.capitalize()
+    
+    preco_limpo = limpar_preco(preco_sujo)
+    
+    faturamento_total = faturamento_total + preco_limpo
+    print("Cliente:", nome_limpo, "| Valor: R$", preco_limpo)
+    
+print("Faturamento Total usando Funções: R$", faturamento_total)
+    
+    
